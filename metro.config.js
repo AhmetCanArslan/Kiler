c70d24e7b31475c6235172890aae7dbfdf6930a9
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2025-07-04 00:34:19 +0300

    add expo-sqlite dependency and configure metro for SQLite support

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Exclude web platform from building SQLite modules
+config.resolver.platforms = ['ios', 'android', 'native'];
+
+module.exports = config;
